914efb29aff87c009105579e98abfbf63a44c944
Nicolas Fédou <nicolas.fedou@gmail.com>
2015-05-08 12:06:17 +0200

    reset to a kata start

--- src/main/java/org/craftedsw/harddependencies/trip/TripService.java
+++ src/main/java/org/craftedsw/harddependencies/trip/TripService.java
@@ -5,21 +5,27 @@ import java.util.List;
 
 import org.craftedsw.harddependencies.exception.UserNotLoggedInException;
 import org.craftedsw.harddependencies.user.User;
+import org.craftedsw.harddependencies.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getFriendTrips(User loggedUser, User friend) throws UserNotLoggedInException {
-		validate(loggedUser);
-		return (friend.isFriendsWith(loggedUser)) 
-						? findTripsForFriend(friend)
-					    : new ArrayList<Trip>();
-	}
-
-	private void validate(User loggedUser) throws UserNotLoggedInException {
-		if (loggedUser == null) throw new UserNotLoggedInException();
-	}
-
-	protected List<Trip> findTripsForFriend(User friend) {
-		return TripDAO.findTripsByUser(friend);
+	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+		List<Trip> tripList = new ArrayList<Trip>();
+		User loggedUser = UserSession.getInstance().getLoggedUser();
+		boolean isFriend = false;
+		if (loggedUser != null) {
+			for (User friend : user.getFriends()) {
+				if (friend.equals(loggedUser)) {
+					isFriend = true;
+					break;
+				}
+			}
+			if (isFriend) {
+				tripList = TripDAO.findTripsByUser(user);
+			}
+			return tripList;
+		} else {
+			throw new UserNotLoggedInException();
+		}
 	}
 }
