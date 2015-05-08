925d792e08dfce8c1cf51d1695d2fd17a4218bd0
Nicolas Fédou <nicolas.fedou@gmail.com>
2015-05-08 20:30:48 +0200

    test one, isolate singleton

--- src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
+++ src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
@@ -1,6 +1,22 @@
 package org.craftedsw.harddependencies;
 
 
+import org.craftedsw.harddependencies.exception.UserNotLoggedInException;
+import org.craftedsw.harddependencies.trip.TripService;
+import org.craftedsw.harddependencies.user.User;
+import org.junit.Test;
+
+
 public class TripServiceTest {
+	TripService tripService = new TripService() {
+		@Override
+		protected User getLoggedInUser() {
+			return null;
+		}
+	};
+	@Test(expected = UserNotLoggedInException.class)
+	public void shall_throw_exception_when_not_logged_in() throws UserNotLoggedInException {
+		tripService.getTripsByUser(new User());
+	}
 	
 }
