946572690149613a286037c6b8d11927e8aec764
Nicolas Fédou <nicolas.fedou@gmail.com>
2015-05-08 12:28:39 +0200

    objectives

--- src/main/java/org/craftedsw/harddependencies/trip/TripService.java
+++ src/main/java/org/craftedsw/harddependencies/trip/TripService.java
@@ -10,10 +10,14 @@ import org.craftedsw.harddependencies.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+		// not logged user thorw exception and tripList is useless (avoid its init time and gc time)
 		List<Trip> tripList = new ArrayList<Trip>();
+		// test class without testing singleton
 		User loggedUser = UserSession.getInstance().getLoggedUser();
 		boolean isFriend = false;
+		// logged or die is a safe guard (use every where?) and is not really a tripByUser logic a method call could be more explicit
 		if (loggedUser != null) {
+			// finding user friends is not a trip logic !
 			for (User friend : user.getFriends()) {
 				if (friend.equals(loggedUser)) {
 					isFriend = true;
@@ -21,6 +25,7 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
+				// test class without testing other dao (and static call)
 				tripList = TripDAO.findTripsByUser(user);
 			}
 			return tripList;
