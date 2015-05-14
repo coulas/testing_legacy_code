ecba1942ec39521b1edc3ea903174f3d51486832
Nicolas Fédou <nicolas.fedou@gmail.com>
2015-05-14 15:34:00 +0200

    reduce vertical scope of vairables

--- src/main/java/org/craftedsw/harddependencies/trip/TripService.java
+++ src/main/java/org/craftedsw/harddependencies/trip/TripService.java
@@ -8,31 +8,30 @@ import org.craftedsw.harddependencies.user.User;
 import org.craftedsw.harddependencies.user.UserSession;
 
 public class TripService {
-
+	
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = getLoggedInUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsForUser(user);
-			}
-			return tripList;
-		} else {
+		if (loggedUser == null) {
 			throw new UserNotLoggedInException();
 		}
+		boolean isFriend = false;
+		for (User friend : user.getFriends()) {
+			if (friend.equals(loggedUser)) {
+				isFriend = true;
+				break;
+			}
+		}
+		List<Trip> tripList = new ArrayList<Trip>();
+		if (isFriend) {
+			tripList = findTripsForUser(user);
+		}
+		return tripList;
 	}
-
+	
 	protected List<Trip> findTripsForUser(User user) {
 		return TripDAO.findTripsByUser(user);
 	}
-
+	
 	protected User getLoggedInUser() {
 		return UserSession.getInstance().getLoggedUser();
 	}
