abdff6c34a1006d1c9732dd4b3ee5470955476bf
Nicolas Fédou <nicolas.fedou@gmail.com>
2015-05-14 14:31:14 +0200

    first test on easiest execution path

--- src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
+++ src/test/java/org/craftedsw/harddependencies/TripServiceTest.java
@@ -1,6 +1,16 @@
 package org.craftedsw.harddependencies;
 
+import org.craftedsw.harddependencies.exception.UserNotLoggedInException;
+import org.craftedsw.harddependencies.trip.TripService;
+import org.craftedsw.harddependencies.user.User;
+import org.junit.Test;
 
 public class TripServiceTest {
+	final TripService tripService = new TripService();
 	
+	@Test(expected = UserNotLoggedInException.class)
+	public void should_throw_exception_when_not_logged_in() throws Exception {
+		final User anyUser = new User();
+		tripService.getTripsByUser(anyUser);
+	}
 }
